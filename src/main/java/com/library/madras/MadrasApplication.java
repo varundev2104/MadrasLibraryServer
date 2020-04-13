96a09a331122334c538d1f8bc4c01bff93448c8e
unknown <imtaarun@gmail.com>
2020-04-13 12:03:25 +0530

    Updating repo

---
+++ src/main/java/com/library/madras/MadrasApplication.java
@@ -0,0 +1,13 @@
+package com.library.madras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadrasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/library/madras/MadrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.madras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
